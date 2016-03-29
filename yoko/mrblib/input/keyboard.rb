4107a22cdef3467cac09264433c1748225008256
jfcalvo <josefranciscocalvo@gmail.com>
2016-03-29 13:59:24 +0200

    Allow to use symbols for keyboard scancode names

--- yoko/mrblib/input/keyboard.rb
+++ yoko/mrblib/input/keyboard.rb
@@ -3,7 +3,7 @@ module Yoko
     class Keyboard
       class << self
         def key_pressed?(scancode_name)
-          keyboard_state[scancode_name] == true
+          keyboard_state[scancode_name.to_s] == true
         end
 
         def keyboard_state
